a930215d004e6063991da96e5f9ef2a89be5d02d
napatlnwza <khun.064239@gmail.com>
2026-03-11 21:33:55 +0700

    update correct

--- camp-SS/passmoning.py
+++ camp-SS/passmoning.py
@@ -6,29 +6,88 @@ from PIL import Image as PILImage,ImageTk
 
 tk = Tk()
 
+def show_image_correct():
+    ran=random.randint(1,5)
+    if ran == 1:
+        pic=Toplevel(tk)
+        pic.title("guea")
+        img1=PILImage.open('guea.jpg')
+        img=img1.resize((250,250))
+        photo=ImageTk.PhotoImage(img)
+        putphoto=Label(pic,image=photo)
+        putphoto.image=photo
+        putphoto.pack()
+        pic.after(1500, pic.destroy)
+    elif ran == 2:
+        pic=Toplevel(tk)
+        pic.title("win")
+        img=PILImage.open("win.jpg")
+        img=img.resize((250,250))
+        photo=ImageTk.PhotoImage(img)
+        putphoto=Label(pic,image=photo)
+        putphoto.image=photo
+        putphoto.pack()
+        pic.after(1500, pic.destroy)
+    elif ran == 3:
+        pic=Toplevel(tk)
+        pic.title('a-show')
+        img=PILImage.open("ashow.jpg")
+        img=img.resize((250,250))
+        photo=ImageTk.PhotoImage(img)
+        putphoto=Label(pic,image=photo)
+        putphoto.image=photo
+        putphoto.pack()
+        pic.after(1500,pic.destroy)
+    elif ran == 4:
+        pic=Toplevel(tk)
+        pic.title("teng")
+        img=PILImage.open("teng.jpeg")
+        img=img.resize((250,250))
+        photo=ImageTk.PhotoImage(img)
+        putphoto=Label(pic,image=photo)
+        putphoto.image=photo
+        putphoto.pack()
+        pic.after(1500,pic.destroy)
+    elif ran == 5:
+        pic=Toplevel(tk)
+        pic.title("aum")
+        img=PILImage.open("plengsui.jpg")
+        img=img.resize((250,250))
+        photo=ImageTk.PhotoImage(img)
+        putphoto=Label(pic,image=photo)
+        putphoto.image=photo
+        putphoto.pack()
+        pic.after(1500,pic.destroy)
+
+def show_image_incorect():
+    ran=random.randint(1,5)
+
+
 def check():
     password = input1.get()
 
     if password == "1234":
-        # resetscreen()
         setup(width=600, height=600)
         title("")
         penup()
         hideturtle()
-        sety(-100)
-        setx(-100)
-        speed(2)
+        sety(-200)
+        setx(-75)
         color('green')
+        write("Good Boy!",move=True,font=('Arial',26))
+        sety(50)
+        setx(-100)
+        speed(4)
         pensize(60)
+        show_image_correct()
         pendown()
-        write(text="Good Boy!",move=True,font=('Arial',21))
         right(45)
         forward(75)
         left(90)
         forward(300)
         tk.after(1500, lambda: bye())
+        input1.delete(0,END)
     else:
-        # resetscreen()
         setup(width=600, height=600)
         title("")
         penup()
@@ -36,7 +95,7 @@ def check():
         setx(-100)
         sety(100)
         right(45)
-        speed(2)
+        speed(5)
         color('red')
         pendown()
         forward(350)
@@ -60,7 +119,7 @@ ling2=ling.resize((300,300))
 mon=ImageTk.PhotoImage(ling2)
 putling=Label(tk,image=mon)
 
-pleng1=PILImage.open('plengsui.jpg')
+pleng1=PILImage.open('aum.jpg')
 pleng2=ImageTk.PhotoImage(pleng1.resize((300,300)))
 putpleng=Label(tk,image=pleng2)
 
